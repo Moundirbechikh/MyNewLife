5094c34675881afb480c8c7053c3341d2e2f7822
Moundirbechikh <moundirbechikh03@gmail.com>
2025-08-11 22:15:55 +0100

    Update DayObjectives.jsx

--- frontend/src/components/DayObjectives.jsx
+++ frontend/src/components/DayObjectives.jsx
@@ -7,7 +7,7 @@ function DayObjectives({ date }) {
   useEffect(() => {
     const fetchObjectives = async () => {
       try {
-        const res = await fetch('${process.env.REACT_APP_API_URL}/api/objectives', {
+        const res = await fetch('https://mynewlife.onrender.com/api/objectives', {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
