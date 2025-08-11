2bf97cb76ffc3a52a2aabd3a679eb897306def4f
Moundirbechikh <moundirbechikh03@gmail.com>
2025-08-11 15:24:37 +0100

    Update Object.jsx

--- frontend/src/components/Object.jsx
+++ frontend/src/components/Object.jsx
@@ -31,7 +31,7 @@ const Object = ({ theme = 'lime' }) => {
   useEffect(() => {
     const fetchObjectives = async () => {
       try {
-        const res = await fetch('http://localhost:5000/api/objectives', {
+        const res = await fetch('https://mynewlife.onrender.com/api/objectives', {
           headers: {
             Authorization: `Bearer ${localStorage.getItem('token')}`,
           },
