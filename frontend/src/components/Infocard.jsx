6780b9b2752c33dc5683f7b8b5014db87ef16500
Moundirbechikh <moundirbechikh03@gmail.com>
2025-08-12 16:44:45 +0100

    Update Infocard.jsx

--- frontend/src/components/Infocard.jsx
+++ frontend/src/components/Infocard.jsx
@@ -6,7 +6,7 @@ function Infocard({ title, theme = 'lime', level = 300, discription, Icon }) {
   const textColor = getTextColor(theme, level);
 
   return (
-    <div className={`w-[44%] h-64 rounded-xl shadow-lg flex flex-col items-center justify-center sm:text-4xl text-xl font-semibold p-2 ${bgColor} ${textColor} hover:scale-105 transition-transform duration-300`}>
+    <div className={`w-[48%] se:w-[44%] h-64 rounded-xl shadow-lg flex flex-col items-center justify-center sm:text-4xl text-xl font-semibold p-2 ${bgColor} ${textColor} hover:scale-105 transition-transform duration-300`}>
       {Icon && <Icon className="sm:text-5xl text-3xl mb-2 text-black" />}
       <div className=" md:text-4xl text-2xl mb-2 text-black text-center">{title}</div>
       <div className="text-base md:text-xl text-center text-black">{discription}</div>
